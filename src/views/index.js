3d22a2571afeb1a5cf49ef482d5acd45bdfb4876
Gabriel <gabi_borges01@hotmail.com>
2020-11-09 00:00:14 -0300

    Ajustando os exports gerais das Views

--- src/views/index.js
+++ src/views/index.js
@@ -1,6 +1,4 @@
-export { default as Account } from './Account';
 export { default as Dashboard } from './Dashboard';
-export { default as Icons } from './Icons';
 export { default as NotFound } from './NotFound';
 export { default as SignIn } from './SignIn';
-export { default as UserList } from './UserList';
+export { default as TarefaList } from './TarefaList';
