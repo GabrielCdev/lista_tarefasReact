d2470edd4285de921d7e158dff7b76b10a6eba6d
Gabriel <gabi_borges01@hotmail.com>
2020-11-08 23:52:05 -0300

    Alterando os exports de tarefas

--- src/views/UserList/components/index.js
+++ src/views/UserList/components/index.js
@@ -1,2 +1,2 @@
-export { default as UsersTable } from './UsersTable';
-export { default as UsersToolbar } from './UsersToolbar';
+export { default as TarefasTable } from './TarefasTable';
+export { default as TarefasToolbar } from './TarefasToolbar';
