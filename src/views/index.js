feb45015588b9914d31b953e245dc8a43661ea8e
Gabriel <gabi_borges01@hotmail.com>
2020-11-08 23:26:49 -0300

    Template Material UI. Customização de template. Adicionando tarefas e fazendo select no menu de categorias.

---
+++ src/views/index.js
@@ -0,0 +1,6 @@
+export { default as Account } from './Account';
+export { default as Dashboard } from './Dashboard';
+export { default as Icons } from './Icons';
+export { default as NotFound } from './NotFound';
+export { default as SignIn } from './SignIn';
+export { default as UserList } from './UserList';
